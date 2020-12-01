43c7c0a24012a06b3e6423290d1b81967a761b4a
Mateus Fratini <V0rr@users.noreply.github.com>
2020-12-01 20:28:59 -0300

    Adições

--- fortnight03/cpu/basiccpu/BasicCPU.cpp
+++ fortnight03/cpu/basiccpu/BasicCPU.cpp
@@ -122,11 +122,11 @@ int BasicCPU::ID()
 		case 0x1A000000:
 			return decodeDataProcReg();
 			break;
-		
-		// TODO
-		// implementar o GRUPO A SEGUIR
-		//
 		// x111 Data Processing -- Scalar Floating-Point and Advanced SIMD on page C4-288
+		case 0x1E000000:
+		case 0x0E000000:
+			return decodeDataProcFloat();
+			break;
 
 		
 		// ATIVIDADE FUTURA
@@ -314,6 +314,39 @@ int BasicCPU::decodeDataProcFloat() {
 	// operação executadas pelas instruções acima.
 	switch (IR & 0xFF20FC00)
 	{
+		case 0x1E202800:
+			// C7.2.43 FADD (scalar)
+
+			if (IR & 0x00C00000) return 1;
+
+			fpOp = FPOpFlag::FP_REG_32;
+			
+			// ler A e B
+			n = (IR & 0x000003E0) >> 5;
+			A = getSasInt(n); // 32-bit variant
+
+			m = (IR & 0x001F0000) >> 16;
+			B = getSasInt(m);
+
+			// registrador destino
+			d = (IR & 0x0000001F);
+			Rd = &(V[d]);
+			
+			// atribuir ALUctrl
+			ALUctrl = ALUctrlFlag::ADD;
+			
+			// atribuir MEMctrl
+			MEMctrl = MEMctrlFlag::MEM_NONE;
+			
+			// atribuir WBctrl
+			WBctrl = WBctrlFlag::RegWrite;
+			
+			// atribuir MemtoReg
+			MemtoReg = false;
+			
+			return 0;
+
+
 		case 0x1E203800:
 			//C7.2.159 FSUB (scalar) on page C7-1615
 			
@@ -381,6 +414,9 @@ int BasicCPU::EXI()
 	{
 		case ALUctrlFlag::SUB:
 			ALUout = A - B;
+			return 0;
+		case ALUctrlFlag::ADD:
+			ALUout = A + B;
 			return 0;
 		default:
 			// Controle não implementado
